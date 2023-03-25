da33badd537854247f0b7c4cdbe8a4b645a18628
alfonsoayalapaloma <alfonsoayalapaloma@gmail.com>
2023-03-25 00:31:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
-#module "compute-engine" {
-#  source = "./modules/compute-engine"
-#}
 module "storage" {
   source = "./modules/storage"
   airflow_rg_name="${var.airflow_rg_name}"
   airflow_sablob_name="${var.airflow_sablob_name}"
 }
-module "sqlserver" {
-  source = "./modules/sqlserver"
-  airflow_rg_name="${var.airflow_rg_name}"
-  airflow_sa_name="${var.airflow_sa_name}"
-  airflow_sqls_name="${var.airflow_sqls_name}"
-  airflow_sqls_dbname="${var.airflow_sqls_dbname}"
-}
+#module "compute-engine" {
+#  source = "./modules/compute-engine"
+#}  
+#module "sqlserver" {
+#  source = "./modules/sqlserver"
+#  airflow_rg_name="${var.airflow_rg_name}"
+#  airflow_sa_name="${var.airflow_sa_name}"
+#  airflow_sqls_name="${var.airflow_sqls_name}"
+#  airflow_sqls_dbname="${var.airflow_sqls_dbname}"
+#}
 #module "cosmosdb" {
 # # source = "./modules/cosmosdb"
 #}
