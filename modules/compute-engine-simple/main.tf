a2a74ba9c392df3530b4189bd6bd6333741b914e
Alfonso Ayala <alfonsoayalapaloma@gmail.com>
2023-04-20 16:39:05 +0000

    compute-engine-simple update

--- modules/compute-engine-simple/main.tf
+++ modules/compute-engine-simple/main.tf
@@ -1,53 +1,46 @@
-variable "prefix" {
-  default = "tfvmex"
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
 }
 
-locals {
-  virtual_machine_name = "${var.prefix}vm"
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+ # name     = random_pet.rg_name.id
+  name     = var.resource_group_name 
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "${var.airflow_rg_name}"  
-  location = "West Europe"
+# Create virtual network
+resource "azurerm_virtual_network" "my_terraform_network" {
+  name                = "myVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "${var.prefix}-network"
-  address_space       = ["172.16.0.0/16"]
-  resource_group_name = "${azurerm_resource_group.example.name}"
-  location            = "${azurerm_resource_group.example.location}"
+# Create subnet
+resource "azurerm_subnet" "my_terraform_subnet" {
+  name                 = "mySubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_subnet" "external" {
-  name                 = "external"
-  virtual_network_name = "${azurerm_virtual_network.example.name}"
-  resource_group_name  = "${azurerm_resource_group.example.name}"
-  address_prefixes     = ["172.16.1.0/24"]
-}
-
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  virtual_network_name = "${azurerm_virtual_network.example.name}"
-  resource_group_name  = "${azurerm_resource_group.example.name}"
-  address_prefixes     = ["172.16.2.0/24"]
-}
-
-resource "azurerm_public_ip" "example" {
-  name                = "${var.prefix}-pip"
-  location            = "${azurerm_resource_group.example.location}"
-  resource_group_name = "${azurerm_resource_group.example.name}"
+# Create public IPs
+resource "azurerm_public_ip" "my_terraform_public_ip" {
+  name                = "myPublicIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
 }
 
-resource "azurerm_network_security_group" "example" {
-  name                = "${var.prefix}-nsg"
-  location            = "${azurerm_resource_group.example.location}"
-  resource_group_name = "${azurerm_resource_group.example.name}"
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "my_terraform_nsg" {
+  name                = "myNetworkSecurityGroup"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "allow_SSH"
-    description                = "Allow SSH access"
-    priority                   = 100
+    name                       = "SSH"
+    priority                   = 1001
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
@@ -56,76 +49,59 @@ resource "azurerm_network_security_group" "example" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-
-  security_rule {
-    name                       = "allow_RDP"
-    description                = "Allow RDP access"
-    priority                   = 110
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "3389"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
 }
 
-resource "azurerm_network_interface" "external" {
-  name                      = "${var.prefix}-ext-nic"
-  location                  = "${azurerm_resource_group.example.location}"
-  resource_group_name       = "${azurerm_resource_group.example.name}"
+# Create network interface
+resource "azurerm_network_interface" "my_terraform_nic" {
+  name                = "myNIC"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "primary"
-    subnet_id                     = "${azurerm_subnet.external.id}"
+    name                          = "my_nic_configuration"
+    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = "${azurerm_public_ip.example.id}"
+    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
   }
 }
 
-resource "azurerm_network_interface" "internal" {
-  name                = "${var.prefix}-int-nic"
-  location            = "${azurerm_resource_group.example.location}"
-  resource_group_name = "${azurerm_resource_group.example.name}"
-
-  ip_configuration {
-    name                          = "primary"
-    subnet_id                     = "${azurerm_subnet.internal.id}"
-    private_ip_address_allocation = "Dynamic"
-  }
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.my_terraform_nic.id
+  network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
 }
 
-resource "azurerm_virtual_machine" "main" {
-  name                          = "${var.prefix}-vm"
-  location                      = "${azurerm_resource_group.example.location}"
-  resource_group_name           = "${azurerm_resource_group.example.name}"
-  primary_network_interface_id  = "${azurerm_network_interface.external.id}"
-  network_interface_ids         = ["${azurerm_network_interface.external.id}", "${azurerm_network_interface.internal.id}"]
-  vm_size                       = "Standard_DS1_v2"
-  delete_os_disk_on_termination = true
 
-  storage_image_reference {
-    publisher = "OpenLogic"
-    offer     = "CentOS"
-    sku       = "8_5-gen2"
-    version   = "latest"
+# Create (and display) an SSH key
+resource "tls_private_key" "example_ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Create virtual machine
+resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
+  name                  = "myVM"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
+  size                  = var.machine_size  #"Standard_DS1_v2"
+
+  os_disk {
+    name                 = "myOsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
   }
 
-  storage_os_disk {
-    name              = "${local.virtual_machine_name}-osdisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
+  source_image_reference {
+    publisher = var.image_publisher #"Canonical"
+    offer     = var.image_offer     #"0001-com-ubuntu-server-jammy"
+    sku       = var.image_sku       #"22_04-lts-gen2"
+    version   = var.image_version   #"latest"
   }
 
-  os_profile {
-    computer_name  = "${local.virtual_machine_name}"
-    admin_username = "myadmin"
-    admin_password = "Passwword1234"
-  }
+  computer_name                   = var.machine_name #"myvm"
+  admin_username                  = "azureuser"
+  admin_password = "Passwword1234"
+  disable_password_authentication = false
 
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
 }
--- modules/compute-engine-simple/variables.tf
+++ modules/compute-engine-simple/variables.tf
@@ -1,3 +1,56 @@
 variable "airflow_rg_name" {
-  type = string
+  type        = string
+  default     = "airflow"
+  description = "Resource group Name. "
+}
+
+variable "resource_group_name" {
+  type        = string
+  default     = "airflow"
+  description = "Resource group Name. "
+}
+
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "rg"
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable "machine_size" {
+  type        = string 
+  default     = "Standard_B1s"
+  description = "Virtual machine size"
+}
+
+variable "machine_name" {
+  type        = string 
+  default     = "myVM"
+  description = "Virtual machine name"
+}
+
+
+variable "image_publisher"{
+   type       = string
+   default    = "redhat"
+}
+
+variable "image_offer"{
+   type       = string
+   default    = "RHEL"
+}
+
+variable "image_sku"{
+   type       = string
+   default    = "7-RAW"
+}
+
+variable "image_version"{
+   type       = string
+   default    = "latest"
 }
