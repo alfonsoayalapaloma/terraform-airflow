c312d6d908feea63ebb4f5e8bb6a5b3b1370b244
alfonsoayalapaloma <alfonsoayalapaloma@gmail.com>
2023-04-21 16:03:58 -0500

    Update main.tf

--- modules/compute-engine-simple/main.tf
+++ modules/compute-engine-simple/main.tf
@@ -49,6 +49,17 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  security_rule {
+    name                       = "FLASK"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 # Create network interface
